86cf30c7dde7ab8a8849f292672743072b366097
Anastayshass <nastiasuvorkina03@gmail.com>
2025-01-22 23:02:05 +0300

    Update Lab 2 (Book).py

--- Lab 2 (Book).py
+++ Lab 2 (Book).py
@@ -1,26 +1,30 @@
+from typing import Optional
+
 class Book:
-    def __init__(self, id: int, name: str, pages: int):
-        self.id = id
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
         self.name = name
         self.pages = pages
     def __str__(self):
         return f'Название книги "{self.name}"'
-
     def __repr__(self):
-        return f"Book(id ={self.id}, name='{self.name}', pages={self.pages})"
+        return f"Book(id_={self.id_}, name='{self.name}', pages={self.pages})"
 
 class Library:
-    def __init__(self, books = []):
+    def __init__(self, books: Optional[list[Book]] = None):
+        if books is None:
+            books = []
         self.books = books
-    def get_next_book_id(self):
-        if len(self.books) == 0:
+    def get_next_book_id(self) -> int:
+        if not self.books:
             return 1
-        else:
-            return self.books[-1].id+1
-    def get_index_by_book_id(self, id: int):
-        for i in self.books:
-            if i.id == id:
-                return self.books.index(i)
+        last_book = self.books[-1]
+        next_id = last_book.id_ + 1
+        return next_id
+    def get_index_by_book_id(self, id_: int) -> int:
+        for index, book in enumerate(self.books):
+            if book.id_ == id_:
+                return index
         raise ValueError("Книги с запрашиваемым id не существует")
 
 if __name__ == "__main__":
